ea456112eaed6cc03c80ac9276a4af59cd99ccc3
WWWietske <WWWietske@users.noreply.github.com>
2025-12-07 22:15:09 +0100

    Antwoordberichten aangepast

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Object met codes en symbolen
 const dayData = {
     7:   { code: "313",   symbol: "sok.png",          nextSymbol: "thee.png",     answer: "Raadsel elf wenst jullie succes! Onthoud de letter D.",                     hint: "Zoek iets warms om te drinken..." },
-    8:   { code: "432",    symbol: "thee.png",         nextSymbol: "kerstbal.png", answer: "Zoek bij de theepot in de keuken.",                      hint: "Kijk in de kerstboom..." },
-    9:   { code: "Londen",     symbol: "kerstbal.png",     nextSymbol: "zuurstok.png", answer: "De kerstbal hangt aan de linkerkant van de boom.",        hint: "Zoek iets zoets..." },
-    10:  { code: "21-19-36864",      symbol: "zuurstok.png",     nextSymbol: "takblaadje.png", answer: "De zuurstok ligt in de snoepkom.",                       hint: "Kijk naar een plant..." },
-    11:  { code: "10100111111000",    symbol: "takblaadje.png",   nextSymbol: "huis.png",      answer: "Het blaadje ligt bij de kerstkrans aan de deur.",         hint: "Zoek een mini-huisje..." },
-    12:  { code: "in een cocktail",    symbol: "huis.png",         nextSymbol: "truistrik.png", answer: "Het kersthuisje staat op de schouw.",                    hint: "Kijk onder de kersttrui..." },
-    13:  { code: "watjjus",      symbol: "truistrik.png",    nextSymbol: "truiboom.png", answer: "De strik zit aan de kersttrui in de kast.",              hint: "Zoek een kerstboom van papier..." },
-    14:  { code: "krans",     symbol: "truiboom.png",     nextSymbol: "ijsbeer.png",  answer: "De papieren kerstboom hangt aan de koelkast.",           hint: "Zoek een dier dat van kou houdt..." },
-    15:  { code: "vier eieren",   symbol: "ijsbeer.png",      nextSymbol: "pinguin.png",  answer: "De ijsbeer staat op de boekenplank.",                    hint: "Zoek een zwart-wit dier..." },
-    16:  { code: "2021",     symbol: "pinguin.png",      nextSymbol: "sneeuwpop.png", answer: "De pinguïn staat naast de tv.",                         hint: "Bouw iets met sneeuw..." },
-    17:  { code: "Helaas nog geen cadeau vandaag jullie doen goed je best iedere dag wat dichterbij ga zo door joe",     symbol: "sneeuwpop.png",    nextSymbol: "vosje.png",    answer: "De sneeuwpop staat in de tuin (of op een foto).",        hint: "Zoek een sluwe vos..." },
-    18:  { code: "christmas movie",     symbol: "vosje.png",        nextSymbol: "koekemannetje.png", answer: "Het vosje zit verstopt in de kerststal.",               hint: "Zoek iets lekkers om op te eten..." },
-    19:  { code: "5",      symbol: "koekemannetje.png", nextSymbol: "boompjes.png", answer: "Het koekemannetje hangt aan het raam.",                hint: "Kijk naar kleine boompjes..." },
-    20:  { code: "4524",     symbol: "boompjes.png",      nextSymbol: "takjebessen.png", answer: "De mini-boompjes staan op de vensterbank.",            hint: "Zoek rode bessen..." },
-    21:  { code: "sneeuwpop",     symbol: "takjebessen.png",  nextSymbol: "wanten.png",   answer: "De tak met bessen ligt op tafel.",                     hint: "Houd je handen warm..." },
-    22:  { code: "The Twelve Days of Christmas",    symbol: "wanten.png",       nextSymbol: "muts.png",     answer: "De wanten hangen bij de deur.",                         hint: "Zet iets op je hoofd..." },
-    23:  { code: "Haha jullie zijn er nog steeds niet",     symbol: "muts.png",          nextSymbol: "cadeau.png",  answer: "De muts ligt op de bank.",                              hint: "Morgen is het kerst!" },
-    24:  { code: "Open de dubbele bodem",     symbol: "cadeau.png",       nextSymbol: "",           answer: "Gefeliciteerd! Het laatste cadeau ligt onder de tafel. Proost! 🎄🎁", hint: "" }
+    8:   { code: "432",    symbol: "thee.png",         nextSymbol: "kerstbal.png", answer: "Dat was even puzzelen. Onthoud de letter D.",                      hint: "Kijk in de kerstboom..." },
+    9:   { code: "Londen",     symbol: "kerstbal.png",     nextSymbol: "zuurstok.png", answer: "De volgend WATJJUS trip staat op de planning, goed geraden! Onthoud de letter E.",        hint: "Zoek iets zoets..." },
+    10:  { code: "21-19-36864",      symbol: "zuurstok.png",     nextSymbol: "takblaadje.png", answer: "Knap de logica doorgrond. Onthoud de letter O.",                       hint: "Kijk naar een plant..." },
+    11:  { code: "10100111111000",    symbol: "takblaadje.png",   nextSymbol: "huis.png",      answer: "Pfoe, wat een lange code was dat. Onthoud de letter E.",         hint: "Zoek een mini-huisje..." },
+    12:  { code: "in een cocktail",    symbol: "huis.png",         nextSymbol: "truistrik.png", answer: "Ik kan er ook wel eentje gebruiken. Onthoud de letter P.",                    hint: "Wat een mooie trui..." },
+    13:  { code: "watjjus",      symbol: "truistrik.png",    nextSymbol: "truiboom.png", answer: "Dit moest natuurlijk een dag het antwoord zijn. Onthoud de letter B.",              hint: "Gelukkig is er nog een trui..." },
+    14:  { code: "krans",     symbol: "truiboom.png",     nextSymbol: "ijsbeer.png",  answer: "Hangt deze bij jullie op de deur? Onthoud de letter D.",           hint: "Zoek een dier dat van kou houdt..." },
+    15:  { code: "vier eieren",   symbol: "ijsbeer.png",      nextSymbol: "pinguin.png",  answer: "Lekker voor het ontbijt zeg. Onthoud de letter E.",                    hint: "Zoek een zwart-wit dier..." },
+    16:  { code: "2021",     symbol: "pinguin.png",      nextSymbol: "sneeuwpop.png", answer: "Ahhh de goede ouwe tijd. Onthoud de letter N.",                         hint: "Bouw iets met sneeuw..." },
+    17:  { code: "Helaas nog geen cadeau vandaag jullie doen goed je best iedere dag wat dichterbij ga zo door joe",     symbol: "sneeuwpop.png",    nextSymbol: "vosje.png",    answer: "Sorry voor het hele lange antwoord. Onthoud de letter B.",        hint: "Zoek een sluwe vos..." },
+    18:  { code: "christmas movie",     symbol: "vosje.png",        nextSymbol: "koekemannetje.png", answer: "Welke is jullie favoriet? Onthoud de letter L.",               hint: "Zoek iets lekkers om op te eten..." },
+    19:  { code: "5",      symbol: "koekemannetje.png", nextSymbol: "boompjes.png", answer: "Deze puzzel viel wel mee, toch? Onthoud de letter O.",                hint: "Kijk naar kleine boompjes..." },
+    20:  { code: "4524",     symbol: "boompjes.png",      nextSymbol: "takjebessen.png", answer: "Knap gedaan! Onthoud de letter B.",            hint: "Zoek rode bessen..." },
+    21:  { code: "sneeuwpop",     symbol: "takjebessen.png",  nextSymbol: "wanten.png",   answer: "Jammer dat we die niet hebben kunnen maken de afgelopen jaren. Onthoud de letter E.",                     hint: "Houd je handen warm..." },
+    22:  { code: "The Twelve Days of Christmas",    symbol: "wanten.png",       nextSymbol: "muts.png",     answer: "Mooi nummer toch! Onthoud de letter E.",                         hint: "Zet iets op je hoofd..." },
+    23:  { code: "Haha jullie zijn er nog steeds niet",     symbol: "muts.png",          nextSymbol: "cadeau.png",  answer: "Hihi, succes nog eventjes! Onthoud de letter M.",                              hint: "Morgen is het kerst!" },
+    24:  { code: "Open de dubbele bodem",     symbol: "cadeau.png",       nextSymbol: "",           answer: "Gefeliciteerd! Het is tijd om het cadeau'tje te pakken. Zoek goed in de doos 🎄🎁", hint: "" }
 };
 
 // Huidige datum bepalen
